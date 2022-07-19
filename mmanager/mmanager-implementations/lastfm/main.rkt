7600e34757e8658ee7433e32707b64d343888b3c
semenInRussia <hrams205@gmail.com>
2022-07-19 23:37:20 +0300

    Tests for last.fm from tst.rkt to main.rkt

---
+++ mmanager/mmanager-implementations/lastfm/main.rkt
@@ -0,0 +1,36 @@
+#lang racket/base
+
+(require
+ rackunit
+ racket/unit
+ mmanager-core
+ "./lastfm-artists-unit.rkt"
+ "./lastfm-helper.rkt")
+
+;; Tests for artists
+(module+ test
+  (define-values/invoke-unit/infer lastfm-artists@)
+
+  (check-pred lastfm-artist? (get-artist (make-artist-model "ac-dc")))
+
+  (check-equal?
+   (lastfm-artist-name (get-artist (make-artist-model "ac-dc")))
+   "AC/DC")
+
+  (check-equal?
+   (artist-url (get-artist (make-artist-model "ac-dc")))
+   "https://www.last.fm/music/AC%2FDC")
+
+  (check-equal?
+   (artist-img-url (get-artist (make-artist-model "ac-dc")))
+   "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png")
+
+  (let ([albums (artist-albums (get-artist (make-artist-model "ac-dc")))])
+    (check-true
+     (andmap lastfm-album? albums)))
+
+  (let ([tracks (artist-tracks-top (get-artist (make-artist-model "ac-dc")))])
+    (check-true
+     (andmap lastfm-track? tracks))))
+
+
